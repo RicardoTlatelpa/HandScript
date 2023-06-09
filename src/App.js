bf985c03f98080baf4e7b8c24f0391af86decdd7
Connie Qiu <connieqiu24@gmail.com>
2023-06-09 16:52:29 -0700

    added a prompt for the user telling them what letter to write, not fully correct yet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
         <>
         <h1> HandScript </h1>
         <MyCanvas/>
+        <WriteLetterPrompt/>
         </>
     )
 }
@@ -13,6 +14,17 @@ const MyCanvas = () => {
     return(
         <canvas id = "myCanvas" width = "500" height = "300" style={{border:"1px solid #000000"}}></canvas>
     )
+}
+
+//Not yet finished need to change it so every time the user finishes writing a letter it moves to next letter. Maybe add a next letter button?
+const WriteLetterPrompt = () => {
+    const alphabet = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H' , 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', "Y", 'z', 'Z'];
+    for(let i = 0; i < alphabet.length - 1; i++){
+        let letter = alphabet[i];
+        return(
+        <p id = "write-letter-prompt"> Write the letter: {letter}</p>
+        )
+    }
 }
 
 export default App;
