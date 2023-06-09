86230aa74e639fbba88422e624e56b15c07db52e
Connie Qiu <connieqiu24@gmail.com>
2023-06-09 16:00:09 -0700

    added HTML canvas element with border and changed importing ReactDom to import from react-dom/client

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const App = () => {
     return(
+        <>
         <h1> HandScript </h1>
+        <MyCanvas/>
+        </>
+    )
+}
+
+const MyCanvas = () => {
+    return(
+        <canvas id = "myCanvas" width = "500" height = "300" style={{border:"1px solid #000000"}}></canvas>
     )
 }
 
