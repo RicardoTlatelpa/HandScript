6f79e3e04b6cd2c9264e9bbef553a76b451c7d0e
Connie Qiu <connieqiu24@gmail.com>
2023-06-26 13:55:48 -0700

    rewrote entire canvas to use Fabric.js library and added Next Letter Button which will convert the drawn image to svg when clicked and clears the canvas to prepare for next letter

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,89 +1,54 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { fabric } from 'fabric';
 import ClearButton from './ClearButton';
 
+
 const Canvas = () => {
-    const canvasRef = useRef(null);
-    const contextRef = useRef(null);
-
-    const [isDrawing, setDrawingOrNot] = useState(false);
-    const [clearCanvas, setClearCanvas] = useState(false);
-
-    const handleClearCanvas = () => {
-        console.log("handled");
-        setClearCanvas(!clearCanvas);
-    };
-
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        contextRef.current = context;
-        context.lineCap = 'round';
-        context.strokeStyle = 'black';
-        context.lineWidth = 3;
-
-        const canvasElement = canvasRef.current;
-
-        canvasElement.addEventListener('mousedown', startDrawing);
-        canvasElement.addEventListener('mousemove', drawOnCanvas);
-        canvasElement.addEventListener('mouseup', stopDrawing);
-
+  const canvasRef = useRef(null);
+  let canvas;
+  useEffect(() => {
+    canvas = new fabric.Canvas(canvasRef.current);
     return () => {
-        canvasElement.removeEventListener('mousedown', startDrawing);
-        canvasElement.removeEventListener('mousemove', drawOnCanvas);
-        canvasElement.removeEventListener('mouseup', stopDrawing);
+      canvas.dispose();
     };
-    }, [isDrawing]);
+  }, []);
 
-    useEffect(() => {
-            const canvas = canvasRef.current;
-            const context = canvas.getContext('2d');
-            context.clearRect(0, 0, canvas.width, canvas.height);
-        },[clearCanvas]);
+  useEffect(() => {
+    if(canvas) {
+      canvas.isDrawingMode = true;
+    }
+    canvas.freeDrawingBrush.width = 5;
+  },[]);
 
-    const getCoordinates = (event) => {
-        const canvas = canvasRef.current;
-        const rect = canvas.getBoundingClientRect();
-        const offsetX = event.clientX - rect.left;
-        const offsetY = event.clientY - rect.top;
-        return { offsetX, offsetY };
-    };
+  const letterToSVG = () => {
+    if(canvas) {
+      const svg = canvas.toSVG();
+      console.log(svg);
+      canvas.clear();
+    }
+  };
 
-    const startDrawing = (event) => {
-        const { offsetX, offsetY } = getCoordinates(event);
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(offsetX, offsetY);
-        setDrawingOrNot(true);
-    };
+  const clearCanvas = () => {
+    if(canvas) {
+      canvas.clear();
+    }
+  };
+  
 
-    const drawOnCanvas = (event) => {
-        if (isDrawing) {
-        const { offsetX, offsetY } = getCoordinates(event);
-        contextRef.current.lineTo(offsetX, offsetY);
-        contextRef.current.stroke();
-        }
-    };
-
-    const stopDrawing = () => {
-        contextRef.current.closePath();
-        setDrawingOrNot(false);
-    };
-
-    return (
-        <div>
-            <canvas
-                ref={canvasRef}
-                width={500}
-                height={300}
-                style={{ border: '1px solid #000000' }}
-            ></canvas>
-            <div>
-                <ClearButton onClear={handleClearCanvas}>Clear</ClearButton>
-            </div>
-        </div>
-    );
+  return(
+    <div>
+      <canvas ref={canvasRef} width={"700"} height={"600"} style={{ border: '1px solid black'}} />
+      <div>
+        <ClearButton onClear={clearCanvas} />
+        <button onClick={letterToSVG}>Next Letter</button>
+      </div>
+    </div>
+  );
 };
 
 export default Canvas;
+
+
 
 
 
---
+++ src/NextLetterButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NextLetterButton = ({ onClear, onConvertToSVG}) => {
+    /*const handleClick = () => {
+        onConvertToSVG();
+        onClear();
+    }*/
+    return <button /*onClick={handleClick}*/>Next Letter</button>;
+};
+
+export default NextLetterButton;
