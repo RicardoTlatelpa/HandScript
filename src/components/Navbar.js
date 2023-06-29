b8f1f1fb67a2a2d3c102a3235279c6055a30e59d
Alex Aguirre <kevin.aguirre93@myhunter.cuny.edu>
2023-06-28 23:13:18 -0400

    NavBar fully working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,57 +1,79 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-import {Button} from './Button';
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu =() => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => { 
-        if (window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-    window.addEventListener('resize', showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
-        <nav classname = "navbar">
-            <div classNamev = "navbar-container">
-             <Link to ="/" className = "navbar-logo">
-                HandScript <i className='fab fa-typo3'/>
-             </Link>
-             <div className = 'menu-icon' onClick={handleClick}>
-                <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
-             </div>
-             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                <li className = 'nav-item'>
-                    <Link to='/home' className = 'nav-links' onClick={closeMobileMenu}>
-                        Home
-                    </Link>
-                </li>
-                <li className = 'nav-item'>
-                    <Link to='/contactUs' className = 'nav-links' onClick={closeMobileMenu}>
-                        Help
-                    </Link>
-                </li>
-                <li className = 'nav-item'>
-                    <Link to='/about' className = 'nav-links' onClick={closeMobileMenu}>
-                        About
-                    </Link>
-                </li>
-             </ul>
-             {button && <Button buttonStyle = 'btn--outline'> SIGN UP</Button>}
-            </div>
-        </nav>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            HANDSCRIPT
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Get Started
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                About
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
