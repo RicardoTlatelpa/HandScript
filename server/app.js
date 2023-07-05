ae0f718dc30486bbb570396033460c470d3f817a
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-05 17:19:38 -0400

    Server test implemented with SVGTEST

--- server/app.js
+++ server/app.js
@@ -6,6 +6,18 @@ app.get('/', (req,res)=>{
   res.send('Handscript server');
 })
 
+const svgtofont = require('svgtofont');
+const path = require('path');
+ 
+svgtofont({
+  src: path.resolve(process.cwd(), 'SVGTEST'), // svg path
+  dist: path.resolve(process.cwd(), 'fonts'), // output path
+  fontName: 'svgtofont', // font name
+  css: true, // Create CSS files.
+}).then(() => {
+  console.log('done!');
+});
+
 app.listen(PORT,()=>{
   console.log(`Example app listening on port ${PORT}`)
 });
