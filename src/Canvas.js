2d2ea191bb3aa8036642f991fc603173bf20da5f
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-08 22:39:43 -0400

    Created unicode hash table

--- src/Canvas.js
+++ src/Canvas.js
@@ -15,6 +15,61 @@ const Canvas = () => {
   const canvasRef = useRef(null);
   const canvasObjRef = useRef(null);
 
+  const unicode = {
+    'A': '0x0041',
+    'B': '0x0042',
+    'C': '0x0043',
+    'D': '0x0044',
+    'E': '0x0045',
+    'F': '0x0046',
+    'G': '0x0047',
+    'H': '0x0048',
+    'I': '0x0049',
+    'J': '0x004A',
+    'K': '0x004B',
+    'L': '0x004C',
+    'M': '0x004D',
+    'N': '0x004E',
+    'O': '0x004F',
+    'P': '0x0050',
+    'Q': '0x0051',
+    'R': '0x0052',
+    'S': '0x0053',
+    'T': '0x0054',
+    'U': '0x0055',
+    'V': '0x0056',
+    'W': '0x0057',
+    'X': '0x0058',
+    'Y': '0x0059',
+    'Z': '0x005A',
+    'a': '0x0061',
+    'b': '0x0062',
+    'c': '0x0063',
+    'd': '0x0064',
+    'e': '0x0065',
+    'f': '0x0066',
+    'g': '0x0067',
+    'h': '0x0068',
+    'i': '0x0069',
+    'j': '0x006A',
+    'k': '0x006B',
+    'l': '0x006C',
+    'm': '0x006D',
+    'n': '0x006E',
+    'o': '0x006F',
+    'p': '0x0070',
+    'q': '0x0071',
+    'r': '0x0072',
+    's': '0x0073',
+    't': '0x0074',
+    'u': '0x0075',
+    'v': '0x0076',
+    'w': '0x0077',
+    'x': '0x0078',
+    'y': '0x0079',
+    'z': '0x007A'
+  }
+
   useEffect(() => {
     canvasObjRef.current = new fabric.Canvas(canvasRef.current);
     return () => {
@@ -56,22 +111,6 @@ const Canvas = () => {
         console.error(err);
       })
   }
-  // //handles lower case alphabet letters
-  // const sendSVGtoServer = (svg) => {
-  //   // make a post request to server
-  //   // 1. create file data for server
-  //   const blob = new Blob([svg[0]], { type: 'image/svg+xml' });
-  //   const formData = new FormData();
-  //   formData.append('file', blob, `${alphabet[svg[1]]}.svg`);
-  //   // 2. send data to server
-  //   axios.post('/handleLC', formData)
-  //   .then(response => {
-  //     console.log(response.data); // Log the response from the server
-  //   })
-  //   .catch(error => {
-  //     console.error(error);
-  //   });
-  // }
 
   const letterToSVG = () => {
     console.log(svgArray, usvgArray);
