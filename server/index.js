8b916be9e61a98c8c2d0f2a362c8ee388c1f6214
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-07-08 21:58:56 -0400

    Sending svg information to server, which can now handle the data, and creates a temporary directory on the fly, stores the SVG files, and then svgtofont converts the svg files into font files

--- server/index.js
+++ server/index.js
@@ -2,45 +2,105 @@ const express = require('express');
 const PORT = 3001;
 const app = express()
 const multer = require('multer');
+const fs= require('fs');
+const svgtofont = require('svgtofont');
+const path= require('path'); 
+const fs_extra = require('fs-extra');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+//app.use(bodyParser.json());
+// var temp = require('temp'),
+//     fs   = require('fs'),
+//     util = require('util'),
 
-var storage = multer.diskStorage(
-  {
-      destination: './server/fonts',
-      filename: function ( req, file, cb ) {
-          //req.body is empty...
-          //How could I get the new_file_name property sent from client here?
-          cb( null, file.originalname+ '-' + Date.now()+".svg");
-      }
-  }
-);
+//     exec = require('child_process').exec;
+ 
+// temp.track();
 
-var upload = multer( { storage: storage } );
+//Multer implementation
+// var storage = multer.diskStorage(
+//   {
+//       destination: `./server/${directoryName}`,
+//       filename: function ( req, file, cb ) {
+//           //req.body is empty...
+//           //How could I get the new_file_name property sent from client here?
+//           cb( null, file.originalname+'-'+".svg");
+//       }
+//   }
+// );
+
+//var upload = multer();
 
 app.get('/', (req,res)=>{
   res.send('Handscript server');
 })
 
-app.post('/handleLC',upload.single('file'),(req,res)=>{  
+app.post('/handleLC',bodyParser.json(),(req,res)=>{  
+  //const files = req.files.files;
+  const jsonData = req.body  
+  const directoryName = String(Date.now());
+  const directoryPath = path.join(__dirname, directoryName);
+  
+  // 1.create temporary directory for svgtofont package
+  fs.mkdir(directoryPath, (err) => {
+    if(err) {
+      console.error(err);      
+      return;
+    }      
+  const shipment = jsonData.shipment
+  // 2.retrieve files from client and put into appropriate destination
+  let i = 0
+  shipment.forEach(item=>{
+    const fileName = `ex${i}.svg`;
+    const fileContent = item
+    const filePath = path.join(directoryPath, fileName)
+
+    fs.writeFile(filePath,fileContent, (err)=>{
+      if(err){
+        console.error(err);
+        return;
+      }
+      console.log('File created successfully');
+    })
+    i+=1;
+  })
+  
+  // 3.convert svg to font files
+  svgtofont({
+    src: path.resolve(process.cwd(), `server/${directoryName}`), // svg path
+    dist: path.resolve(process.cwd(), `server/${directoryName}/download`), // output path
+    fontName: 'HandScripts', // font name
+    css: false, // Create CSS files.
+    startUnicode: 0x0041, // unicode start number
+    svgicons2svgfont: {
+      fontHeight:1000,
+      normalize: true
+    },
+  }).then(() => {
+
+    console.log('done!');
+  });
+
+  // 4.send appropriate font files to user
+    
+  // 5.delete any directories created
+  // fs_extra.remove(directoryPath)
+  // .then(()=>{
+  //   console.log('Directory deleted successfully');
+  // })
+  // .catch((err)=>{
+  //   console.error(err);
+  //   res.sendStatus(500);
+  //   return;
+  // });
+  
+  });
+
   res.sendStatus(200);
-} )
+})
 
-// const svgtofont = require('svgtofont');
-// const path = require('path');
- 
-// svgtofont({
-//   src: path.resolve(process.cwd(), 'SVGTEST'), // svg path
-//   dist: path.resolve(process.cwd(), 'fonts'), // output path
-//   fontName: 'svgtofont', // font name
-//   css: true, // Create CSS files.
-//   startUnicode: 0x0030, // unicode start number
-//   svgicons2svgfont: {
-//     fontHeight:1000,
-//     normalize: true
-//   },
-// }).then(() => {
 
-//   console.log('done!');
-// });
 
 app.listen(PORT,()=>{
   console.log(`Example app listening on port ${PORT}`)
--- src/Canvas.js
+++ src/Canvas.js
@@ -5,11 +5,12 @@ import LetterPrompt from './LetterPrompt';
 import './LetterPrompt.css'
 import axios from 'axios';
 const Canvas = () => {
-  //const alphabet = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z'];
-  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const alphabet = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z']; 
+  // const alphabet = ['a','b','c']; // for testing backend
   const [index, setIndex] = useState(0);
   const [currentLetter, setCurrentLetter] = useState(alphabet[index]);
   const [svgArray, setSVG] = useState([]);
+  const [usvgArray, setUSVG] = useState([]);
 
   const canvasRef = useRef(null);
   const canvasObjRef = useRef(null);
@@ -28,32 +29,90 @@ const Canvas = () => {
     }
   },[]);
 
-  const sendSVGtoServer = (svg) => {
-    // make a post request to server
-    // 1. create file data for server
-    const blob = new Blob([svg], { type: 'image/svg+xml' });
-    const formData = new FormData();
-    formData.append('file', blob, `${alphabet[index]}.svg`);
-    // 2. send data to server
-    axios.post('/handleLC', formData)
-    .then(response => {
-      console.log(response.data); // Log the response from the server
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  const checkCase = (ch) =>{
+    if (!isNaN(ch * 1)){
+      return 'ch is numeric';
+   }
+    else {
+      if (ch == ch.toUpperCase()) {
+         return true;
+      }
+      if (ch == ch.toLowerCase()){
+         return false;
+      }
+   }
   }
 
+  const sendJsontoServer = (json) => {    
+    axios.post('/handleLC', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response=>{
+        console.log(response.data)
+      })
+      .catch(err=>{
+        console.error(err);
+      })
+  }
+  // //handles lower case alphabet letters
+  // const sendSVGtoServer = (svg) => {
+  //   // make a post request to server
+  //   // 1. create file data for server
+  //   const blob = new Blob([svg[0]], { type: 'image/svg+xml' });
+  //   const formData = new FormData();
+  //   formData.append('file', blob, `${alphabet[svg[1]]}.svg`);
+  //   // 2. send data to server
+  //   axios.post('/handleLC', formData)
+  //   .then(response => {
+  //     console.log(response.data); // Log the response from the server
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+  // }
+
   const letterToSVG = () => {
+    console.log(svgArray, usvgArray);
+    let lastSVG = '';
     if(canvasObjRef.current && index < alphabet.length){
-      const svg = canvasObjRef.current.toSVG();
-      canvasObjRef.current.clear();
-      addSVG(svg);
-      sendSVGtoServer(svg);
-      nextLetter();
+      lastSVG = canvasObjRef.current.toSVG();
+      canvasObjRef.current.clear();  
+      if(checkCase(alphabet[index])){
+        addUSVG(lastSVG);
+      }else{
+        addSVG(lastSVG);          
+      }
+      nextLetter(); // increments index
+    }
+    if(index === (alphabet.length-1)){
+      console.log("finished")
+      // 1. send svg files to server
+      let lowercaseshipment = []
+      let uppercaseshipment = []
+      let filler =['','','','','',''];
+      usvgArray.map(x => {
+        return uppercaseshipment.push(x);
+      })
+      svgArray.map(x =>{
+        return lowercaseshipment.push(x);
+      })
+      //lowercaseshipment.push(lastSVG); // for lower case tests
+      uppercaseshipment.push(lastSVG);
+      console.log(lowercaseshipment)
+      console.log(uppercaseshipment)
+      console.log(filler)
+      uppercaseshipment = uppercaseshipment.concat(filler);
+      uppercaseshipment = uppercaseshipment.concat(lowercaseshipment);            
+      let json = {shipment: uppercaseshipment}
+      console.log(json);
+      sendJsontoServer(json);
     }
   };
-
+  const addUSVG = (theSVG) => {
+    setUSVG(prevSVGArray => [...prevSVGArray, theSVG]);
+  }
   const addSVG = (theSVG) => {
     setSVG(prevSVGArray => [...prevSVGArray, theSVG]);
   }
