be0e27acc68be430e9d286667404a975d511a883
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-10-05 14:52:54 -0400

    added back complete alphabet to production

--- client/src/Canvas.js
+++ client/src/Canvas.js
@@ -9,8 +9,8 @@ import './Canvas.css'
 
 const Canvas = () => {
   // canvas page states  
-  const alphabet = ['a'];
-  //const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];   
+  //const alphabet = ['a'];
+  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];   
   const [index, setIndex] = useState(0);
   const [currentLetter, setCurrentLetter] = useState(alphabet[index]);
   const [svgArray, setSVG] = useState([]);
